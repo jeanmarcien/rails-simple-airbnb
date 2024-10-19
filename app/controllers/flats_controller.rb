1ec4a97e1e948d9bfc2f014d3dad117a2a6aff31
Jean-Marcien Kablansi <jean.marcien@gmail.com>
2024-10-19 00:18:20 -0400

    Submissions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -22,9 +22,25 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flats_path(@restaurant)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :image_url)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,8 @@
 class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price_per_night, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :number_of_guests, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :image_url, presence: true, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }
 end
